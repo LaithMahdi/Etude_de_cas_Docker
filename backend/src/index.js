52121438b4f1a9ab7ba99db1856c93a2bc266907
LaithMahdi <mahdilaith380@gmail.com>
2023-11-20 21:50:43 +0100

    aading same data

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
-const port =8080;
+const port = 8080;
 
 
-app.get('/',(req,res)=>{
-    res.send('Hello from Express');
+const data = [
+    { id: 1, name: "Five Nights at Freddy's (2023)", description: "Recently fired and desperate for work, a troubled young man named Mike agrees to take a position as a night security guard at an abandoned theme restaurant: Freddy Fazbear's Pizzeria. But he soon discovers that nothing at Freddy's is what it seems." },
+    { id: 2, name: "The Hunger Games: The Ballad of Songbirds & Snakes (2023) ", description: "64 years before he becomes the tyrannical president of Panem, Coriolanus Snow sees a chance for a change in fortunes when he mentors Lucy Gray Baird, the female tribute from District 12." },
+    { id: 3, name: " The Marvels (2023)", description: "Carol Danvers, aka Captain Marvel, has reclaimed her identity from the tyrannical Kree and taken revenge on the Supreme Intelligence. But unintended consequences see Carol shouldering the burden of a destabilized universe. When her duties send her to an anomalous wormhole linked to a Kree revolutionary, her powers become entangled with that of Jersey City super-fan Kamala Khan, aka Ms. Marvel, and Carol’s estranged niece, now S.A.B.E.R. astronaut Captain Monica Rambeau. Together, this unlikely trio must team up and learn to work in concert to save the universe." },
+];
+
+
+app.get('/', (req, res) => {
+    res.json(data);
 });
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is running : http://localhost:${port}`);
 });
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,52 @@
 import Image from 'next/image'
 
-export default function Home() {
+
+const getMovies = async () => {
+  const data = await fetch("http://localhost:8080/");
+  const movies = await data.json();
+  return movies;
+};
+
+interface Movie {
+  id: number,
+  name: String,
+  description: String
+}
+
+
+export default async function Home() {
+  const movies = await getMovies();
+  console.log(movies);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1>Hello from Next</h1>
-      </div>
+    <main className="container mx-auto my-4">
+      <h1 className='mb-2 text-2xl' >Movies</h1>
+      <table className='table-auto border-collapse border border-slate-400'>
+        <thead>
+          <tr>
+            <th className='border border-slate-300 w-12'>ID</th>
+            <th className='border border-slate-300'>Name</th>
+            <th className='border border-slate-300'>Descrption</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((movie: Movie) => (
+            <tr key={movie.id}>
+              <td className='border border-slate-300'>
+                {movie.id}
+              </td>
+
+              <td className='border border-slate-300'>
+                {movie.name}
+              </td>
+
+              <td className='border border-slate-300'>
+                {movie.description}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
     </main>
   )
 }
